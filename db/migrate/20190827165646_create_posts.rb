c4b260dfd195d584c2f31996b6a255177f429335
luis quilho <luis.quilho@gmail.com>
2019-08-27 18:15:23 +0100

    added validations to the model

--- db/migrate/20190827165646_create_posts.rb
+++ db/migrate/20190827165646_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :title
       t.string :image
-      t.text :descritpion
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_163740) do
+ActiveRecord::Schema.define(version: 2019_08_27_165646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_163740) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "image"
-    t.text "descritpion"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
