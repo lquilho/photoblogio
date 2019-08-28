2a5de07f7677f9c3b3690581234d2c3b4153b494
luis quilho <luis.quilho@gmail.com>
2019-08-28 13:26:25 +0100

    Add dotenv - Protect my secret data in .env file

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -54,6 +54,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
