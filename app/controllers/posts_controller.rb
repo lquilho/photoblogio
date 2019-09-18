6bdd6e573767f01c25371db1e30018bbc19b88b7
luis quilho <luis.quilho@gmail.com>
2019-09-18 16:49:24 +0100

    delete post with photos from index page ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,7 +69,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.delete
+    @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
