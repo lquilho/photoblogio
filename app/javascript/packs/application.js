97adca2c8e34db0758880370115a65817b26e3d5
luis quilho <luis.quilho@gmail.com>
2019-10-25 17:53:10 +0100

    added sweet-scroll library. Installed lightbox2 library with yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,18 @@
 import 'bootstrap';
+import 'lightbox2';
+import SweetScroll from 'sweet-scroll';
+document.addEventListener('DOMContentLoaded', () => {
+  const sweetScroll = new SweetScroll({ /* some options */ });
+}, false);
+
 import { initUpdateNavbarOnScroll } from '../components/navbar';
-
 initUpdateNavbarOnScroll();
-//import { initUpdateNavbarOnScroll } from '../components/navbar';
-//initUpdateNavbarOnScroll();
 
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 //require("jquery")
 //require("packs/jquery.mosaic")
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
